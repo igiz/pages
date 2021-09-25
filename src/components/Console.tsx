a185060023688a8e2e1927ec074f3e9b336db489
Ziggy <zygimantasr1992@gmail.com>
2021-09-25 13:51:45 +0100

    Added animation to opening application
    - Cleaned up css a bit too..

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Draggable from "react-draggable";
 import styled from "styled-components";
 import { AppContext } from "../contexts/AppContext";
@@ -11,44 +11,58 @@ const WindowContainer = styled.div`
     border-bottom: 0.2rem solid #36ADAD;
     border-left: 0.2rem solid #36ADAD;
     border-right: 0.2rem solid #36ADAD;
-    width: 100%;
-    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
     background-color: #292323;
+
+    ::-webkit-scrollbar {
+        width: 0.4rem;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #363232;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #575353;
+    }
 `;
 
 const WindowHeader = styled.div`
     display: flex;
     flex-direction: row-reverse;
     background-color: #36ADAD;
-    min-height: 1.5rem;
-    width: 100%;
     padding: 0.2rem;
 `;
 
-const MainContainer = styled.div<{ height: number | string, width: number | string }>`
+const MainContainer = styled.div<{ height: number | string, width: number | string, opacity: number, left: string }>`
     width: ${props => props.width};
     height: ${props => props.height};
     position: relative;
-    left: 15%;
+    left: ${props => props.left};
     top: 10%;
+    transition: opacity 0.5s, left 1s;
+    opacity: ${props => props.opacity};
 `;
 
 const HeaderButton = styled.div`
     display: flex;
-    height: 25px;
-    width: 25px;
+    height: 1.5rem;
+    width: 1.5rem;
     background-color: #292323;
-    border-radius: 20%;
+    border-radius: 0.3rem;
     justify-content: center;
     align-items: center;
     transition: background-color 1s;
     :hover {
         background-color: #000000;
+        cursor: pointer;
     }
 `;
 
 const HeaderTitle = styled.div`
-    width: 97%;
+    width: 100%;
     user-select: none;
 `;
 
@@ -60,8 +74,8 @@ const HeaderText = styled.p`
 `;
 
 const Circle = styled.div`
-  width: 15px;
-  height: 15px;
+  width: 1rem;
+  height: 1rem;
   background-color: #FB9632;
   border-radius: 50%;
   transition: background-color 1s , border-radius 1s, height 1s;
@@ -69,14 +83,15 @@ const Circle = styled.div`
         background-color: #ff0000;
         border-radius: 0;
         height: 7px;
+        cursor: pointer;
   }
 `;
 
 const ConsoleHeader: React.FC<{ appInfo: ProcessInfo }> = ({ appInfo }) => {
     var { dispatch } = useContext(AppContext);
     return <WindowHeader className={"handle"}>
-        <HeaderButton>
-            <Circle onMouseDown={() => dispatch(Actions.Close(appInfo))} />
+        <HeaderButton onMouseDown={() => dispatch(Actions.Close(appInfo))}>
+            <Circle />
         </HeaderButton>
         <HeaderTitle>
             <HeaderText>{appInfo.name}</HeaderText>
@@ -85,8 +100,18 @@ const ConsoleHeader: React.FC<{ appInfo: ProcessInfo }> = ({ appInfo }) => {
 }
 
 const Console: React.FC<{ appInfo: ProcessInfo, dimensions: { height: number | string, width: number | string } }> = ({ appInfo, children, dimensions }) => {
+
+    const [opacity, setOpacity] = useState<number>(0);
+    const [left, setLeft] = useState<string>("0%");
+    useEffect(() => {
+        setTimeout(() => {
+            setOpacity(1);
+            setLeft("15%");
+        }, 50)
+    }, [])
+
     return <Draggable bounds='parent' handle={".handle"}>
-        <MainContainer height={dimensions.height} width={dimensions.width}>
+        <MainContainer height={dimensions.height} width={dimensions.width} opacity={opacity} left={left}>
             <ConsoleHeader appInfo={appInfo} />
             <WindowContainer>
                 {children}
